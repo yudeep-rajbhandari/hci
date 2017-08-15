7027132db0fe5f5ad0b8c10eebc7d052a7c1d92f
yudeep-rajbhandari <endlessveins@gmail.com>
2017-08-15 10:24:13 +0545

    final req

--- public/controller/app.js
+++ public/controller/app.js
@@ -148,8 +148,8 @@ app.controller('signUpController', ['$scope', '$http', 'toaster', '$state', 'pri
             })
         }
     }])
-app.controller('countrycontroller', ['$scope', '$http', '$state', 'service', '$rootScope', '$stateParams', 'toaster','principal','VisualService',
-    function ($scope, $http, $state, service, $rootScope, $stateParams, toaster,principal,VisualService) {
+app.controller('countrycontroller', ['$scope', '$http', '$state', 'service', '$rootScope', '$stateParams', 'toaster','principal','VisualService','$timeout',
+    function ($scope, $http, $state, service, $rootScope, $stateParams, toaster,principal,VisualService,$timeout) {
         $scope.formdata = {};
 
 
@@ -173,12 +173,47 @@ app.controller('countrycontroller', ['$scope', '$http', '$state', 'service', '$r
                 }
             })
         }
+        $scope.csvJson=function(abc){
+
+            var lines=abc.split("\n");
+
+            var result = [];
+
+            var headers=lines[0].split(",");
+
+            for(var i=1;i<lines.length;i++){
+
+                var obj = {};
+                var currentline=lines[i].split(",");
+
+                for(var j=0;j<headers.length;j++){
+                    obj[headers[j]] = currentline[j];
+                }
+
+                result.push(obj);
+
+            }
+
+            //return result; //JavaScript object
+            return JSON.stringify(result); //JSON
+
+        }
+
+
+
+
+            // $scope.csvJson($scope.f);
+
+
+
+
 
 
         $scope.country = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Central African Republic", "Chad", "Chile", "China", "Colombi", "Comoros", "Congo (Brazzaville)", "Congo", "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor (Timor Timur)", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia, The", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, North", "Korea, South", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia and Montenegro", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"];
     }])
 
-app.controller('DataController', ['$scope', '$http', '$state', 'service', 'toaster','$stateParams','VisualService', function ($scope, $http, $state, service, toaster,$stateParams,VisualService) {
+app.controller('DataController', ['$scope', '$http', '$state', 'service', 'toaster','$stateParams','VisualService',
+    function ($scope, $http, $state, service, toaster,$stateParams,VisualService) {
 
     // service.get('/data/findData',function(err,response){
     //
@@ -199,7 +234,6 @@ app.controller('DataController', ['$scope', '$http', '$state', 'service', 'toast
 
 
 
-
     $scope.deletefun = function (data1) {
         console.log(data1);
         if (typeof data1 !== 'undefined' && data1.length > 0) {
@@ -212,7 +246,7 @@ app.controller('DataController', ['$scope', '$http', '$state', 'service', 'toast
                     toaster.pop("Success", "Successfully deleted ");
                     $scope.deleter=[];
                     VisualService.data1();
-                    $state.reload();te
+                    $state.reload();
 
 
                 }
@@ -234,7 +268,7 @@ else {
         console.log($scope.searchdata.data);
         document.getElementById('mover').style.cssText='top:0px;position: fixed;z-index: 100;';
         document.getElementById('titlesearch').style.display='none';
-        document.getElementById('inputid').style.height='25px';
+        document.getElementById('inputid').style.cssText='height=25px;width=200px';
         $scope.tableshow=true;
 
 
