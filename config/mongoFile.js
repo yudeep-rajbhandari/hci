453e50d56951e277e9adfce6bcdd875aafcef2b8
yudeep-rajbhandari <endlessveins@gmail.com>
2021-02-05 11:19:42 +0545

    Update mongoFile.js

--- config/mongoFile.js
+++ config/mongoFile.js
@@ -1,7 +1,7 @@
 
 var mongoose=require('mongoose');
 module.exports=function(app){
-    app.mdb="mongodb+srv://yudeep123:yudeep123>@hci.xht35.mongodb.net/hci?retryWrites=true&w=majority";
+    app.mdb="mongodb://yudeep123:yudeep123@hci-shard-00-00.xht35.mongodb.net:27017,hci-shard-00-01.xht35.mongodb.net:27017,hci-shard-00-02.xht35.mongodb.net:27017/hci?ssl=true&replicaSet=atlas-pem8dx-shard-0&authSource=admin&retryWrites=true&w=majority";
 
     //app.mdb="mongodb://localhost:27017/takeinfood";
     mongoose.set('debug', true);
